fda94ba711e89cd63e7ddfe196e6fc9e49a8b8e7
mikihalamish <61268887+mikihalamish@users.noreply.github.com>
2021-06-02 23:43:48 +0300

    Update DBConfig.js

--- src/Pages/DBConfig.js
+++ src/Pages/DBConfig.js
@@ -17,8 +17,8 @@ const DBConfig = () => {
         axios.post(`http://172.30.171.108:8080/connect`, {
             ip: ip,
             dbName: dbName
-        },{'Content-Type': 'application/x-www-form-urlencoded',
-                    'Accept': 'application/json'}).then((res) =>
+        },{'Content-Type': 'application/json;charset=UTF-8',
+      "Access-Control-Allow-Origin": "*"}).then((res) =>
             axios.post(`http://172.30.171.108:8080/tables`, {
                 dbName: dbName
             }).then((res) => { 

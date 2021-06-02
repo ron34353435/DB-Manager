89da6ea6ed4c86ffee168d79e524721406edf5e1
mikihalamish <61268887+mikihalamish@users.noreply.github.com>
2021-06-02 19:12:02 +0300

    Frontend  Files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DBConfig from './Pages/DBConfig.js'
+
+function App() {
+  return (
+    <div className="App">
+      <DBConfig></DBConfig>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/DBConfig.js
@@ -0,0 +1,37 @@
+import { React, useState, useEffect } from 'react'
+import { TextField, Button } from '@material-ui/core'
+import './dbConfig.css'
+import axios from 'axios'
+
+const DBConfig = () => {
+
+    const [dbName, setDbName] = useState("")
+    const [ip, setIP] = useState("")
+    const [message, setMessage] = useState("לפני חיבור")
+
+    const connectDB = () => {
+        setMessage("מתחבר...")
+        axios.post(`http://${process.env.BACKEND}:8080/connect`, {
+            ip: ip,
+            dbName: dbName
+        }).then((res) =>
+            axios.post(`http://${process.env.BACKEND}:8080/tables`, {
+                dbName: dbName
+            }).then((res) => { 
+                console.log(res)
+                setMessage(JSON.stringify(res.data))
+             })
+        )
+    }
+
+    return (
+        <div id='db-config'>
+            <TextField id="outlined-basic" label="DB Name" variant="outlined" value={dbName} onChange={(e) => setDbName(e.target.value)} />
+            <TextField id="outlined-basic" label="DB IP" variant="outlined" value={ip} onChange={(e) => setIP(e.target.value)} />
+            <Button variant="contained" color="primary" onClick={connectDB}>התחבר</Button>
+            <div>{message}</div>
+        </div>
+    )
+}
+
+export default DBConfig
